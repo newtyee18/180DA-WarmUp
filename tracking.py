a68b2226786cc4ebefe70a9d29b6571e45143d3b
newtyee18 <92614955+newtyee18@users.noreply.github.com>
2022-01-13 17:37:49 -0800

    Added Clustering for Lab1, Added Files Lab2
    For Lab1, I added kmeans clustering to find the dominant colors of a
    frame of my video capture. I essentially do kmeans and then output the
    most dominant colors onto a palette.
    I also started on Lab2 adding the subscriber and publisher code for
    MQTT. The code allows for a subscriber to read messages from a publisher
    that publishes their message onto a "topic." Only members of a "topic"
    can read and write to it.

--- tracking.py
+++ tracking.py
@@ -6,18 +6,45 @@
 # 5) https://gist.github.com/bigsnarfdude/d811e31ee17495f82f10db12651ae82d
 # 6) https://stackoverflow.com/questions/51699162/im-having-error-with-cv2-findcontours
 # 7) https://stackoverflow.com/questions/22588146/tracking-white-color-using-python-opencv
+# 8) https://code.likeagirl.io/finding-dominant-colour-on-an-image-b4e075f98097
 
 from __future__ import print_function
 import cv2 as cv
 import numpy as np
-import argparse
-import random as rng
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+
+def find_histogram(clt):
+    """
+    create a histogram with k clusters
+    :param: clt
+    :return:hist
+    """
+    numLabels = np.arange(0, len(np.unique(clt.labels_)) + 1)
+    (hist, _) = np.histogram(clt.labels_, bins=numLabels)
+
+    hist = hist.astype("float")
+    hist /= hist.sum()
+
+    return hist
+def plot_colors2(hist, centroids):
+    bar = np.zeros((50, 300, 3), dtype="uint8")
+    startX = 0
+
+    for (percent, color) in zip(hist, centroids):
+        # plot the relative percentage of each cluster
+        endX = startX + (percent * 300)
+        cv.rectangle(bar, (int(startX), 0), (int(endX), 50),
+                      color.astype("uint8").tolist(), -1)
+        startX = endX
+
+    # return the bar chart
+    return bar
 
 cap = cv.VideoCapture(0)
 
-lower1 = np.array([70,60,160])          #lower HSV threshold for cyan
-upper1 = np.array([135,255,255])        #upper HSV threshold for cyan
+lower1 = np.array([70,60,160])          # lower HSV threshold for cyan
+upper1 = np.array([135,255,255])        # upper HSV threshold for cyan
 lower2 = np.array([220,220,220])
 upper2 = np.array([255,255,255])
 kernelOpen = np.ones((7,7))
@@ -27,6 +54,7 @@ while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
 
+    # Implementing Kmeans
     # Our operations on the frame come here
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
@@ -40,8 +68,13 @@ while(True):
     mask5 = cv.morphologyEx (rgb_mask, cv.MORPH_OPEN, kernelOpen)
     mask6 = cv.morphologyEx (mask5, cv.MORPH_CLOSE, kernelClose)
     
-    #thresholding the frame
+    # Thresholding the frame for grayscale
     ret,thresh1 = cv.threshold(gray,127,255,cv.THRESH_BINARY)
+
+    # Kmeans for rgb
+    reshaped_rgb = rgb.reshape((rgb.shape[0] * rgb.shape[1], 3)) # comment out if want to get bounding box video
+    clt = KMeans(n_clusters=5) # comment out if want to get bounding box video; kmeans is REALLY SLOW AND WILL LAG VIDEO
+    clt.fit(reshaped_rgb) # comment out if want to get bounding box video
 
     contours, h = cv.findContours(hsv_mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
     for c in contours:
@@ -52,7 +85,12 @@ while(True):
     
     cv.drawContours(hsv, contours, -1, (255, 255, 0), 1)
     # Display the resulting frame
-    cv.imshow('frame', hsv)
+    hist = find_histogram(clt) # comment out if want to get bounding box video
+    bar = plot_colors2(hist, clt.cluster_centers_) # comment out if want to get bounding box video
+    cv.imshow("frame", rgb) # change rgb to hsv to get bounding box video
+    plt.axis("off") # comment out if want to get bounding box video
+    plt.imshow(bar) # comment out if want to get bounding box video
+    plt.show() # comment out if want to get bounding box video
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
 
